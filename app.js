3abea008c2a962dc8d5baf092c2cb76a9d813490
JC <joaobarcelos@gmail.com>
2020-06-13 00:01:14 +0100

    test

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ var commentRoutes    = require("./routes/comments"),
 //local mongodb
 // mongoose.connect("mongodb://localhost/yelp_camp_v11", {useNewUrlParser: true, useUnifiedTopology: true});
 
+//line random
+
 //cloud mongodb atlas
 // mongoose.connect("mongodb+srv://joaobarcelos:HaKpIjK1nnlB9RXk@cluster0-fnitj.mongodb.net/yelp_camp_v12?retryWrites=true&w=majority");
 
